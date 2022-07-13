768f8fad3cd599695ec7c2fb940c9a04337d6a36
zhaqj12 <zhaqj12@naver.com>
2022-07-14 01:46:49 +0900

    Player Camera following-20%

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    UnityEvent<Vector3,Transform> events;
+    [SerializeField]
+    GameObject target;
+
+    float speed = 15.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        movement(Vector3.down,target.transform);
+        else if(Input.GetKeyDown(KeyCode.A))
+            movement(Vector3.up, target.transform);
+
+    }
+
+    private void playerDir()
+    {
+
+        Vector3 TargetPos = new Vector3(
+   transform.position.x + 0,
+    transform.position.y + 3.14f,
+   transform.position.z + -7
+    );
+
+ 
+
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, TargetPos, Time.deltaTime * speed);
+        Camera.main.transform.rotation = transform.rotation;
+
+    }
+    public void movement(Vector3 dir,Transform target)
+    {
+   
+            events?.Invoke(dir,target);
+    }    
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Transform transform;
+
+    public float speed = 2000.5f;
+    private void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+    public void MoveMent(Vector3 dir,Transform target)
+    {
+        transform.RotateAround(target.transform.position, dir, speed * Time.deltaTime);
+    }
+}
