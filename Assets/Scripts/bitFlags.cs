d49256e27739a0ffd6f995464e0175d29575d30f
zhaqj12 <zhaqj12@naver.com>
2022-07-14 23:56:33 +0900

    Git Error 수정

--- Assets/Scripts/bitFlags.cs
+++ Assets/Scripts/bitFlags.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// test 
 public class bitFlags
 {
     [Flags]
--- Assets/Scripts/followCamera.cs
+++ Assets/Scripts/followCamera.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//test 
 public class followCamera : MonoBehaviour
 {
     [SerializeField]
