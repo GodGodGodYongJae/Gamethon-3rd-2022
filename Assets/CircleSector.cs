4ed2ece40a35874114c3c9bea0d6d51e3d010fd2
zhaqj12 <zhaqj12@naver.com>
2022-07-19 17:55:21 +0900

    Attack Range 30%

--- Assets/CircleSector.cs
+++ Assets/CircleSector.cs
@@ -17,12 +17,20 @@ public class CircleSector : MonoBehaviour
     Vector3 direction;
     float dotValue = 0f;
 
+    [Range(3, 256)]
+    public int numSegments = 128;
+    private void Start()
+    {
+        DrawCircle(this.gameObject);
+    }
     private void Update()
     {
         if (EnemyFactoryMethod.Instance?.target)
             target = EnemyFactoryMethod.Instance.target.transform;
         dotValue = Mathf.Cos(Mathf.Deg2Rad * (angleRange / 2));
         direction = target.position - transform.position;
+
+
         if (direction.magnitude < distance)
         {
             if (Vector3.Dot(direction.normalized, transform.forward) > dotValue)
@@ -34,10 +42,40 @@ public class CircleSector : MonoBehaviour
             isCollistion = false;
     }
 
+    public void DrawCircle(GameObject container)
+    {
+
+        var line = container.AddComponent<LineRenderer>();
+        line.SetColors(_red, _red);
+        line.useWorldSpace = false;
+        //line.SetWidth(distance, distance);
+        line.SetVertexCount(numSegments);
+        line.useWorldSpace = false;
+
+            float x;
+            float y;
+            float z = 0f;
+             float angle = 20f;
+
+            for (int i = 0; i < (numSegments + 1); i++)
+            {
+                x = Mathf.Sin(Mathf.Deg2Rad * angle) * 10;
+                y = Mathf.Cos(Mathf.Deg2Rad * angle) * 10;
+
+                line.SetPosition(i, new Vector3(x, y, z));
+
+                angle += (360f / numSegments);
+            }
+        
+    }
     private void OnDrawGizmos()
     {
+        
         Handles.color = isCollistion ? _red : _blue;
-        Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, angleRange/2,distance);
-        Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, -angleRange / 2, distance);
+
+            Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, angleRange / 2, distance);
+            Handles.DrawSolidArc(transform.position, Vector3.up, transform.forward, -angleRange / 2, distance);
+
+      
     }
 }
